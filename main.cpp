0a950fb7c660049f857c267d1120fd8b785a2a9a
denizka <48176611+deethereal@users.noreply.github.com>
2019-11-02 17:54:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,16 +2,16 @@
 //  main.cpp
 //  Gaussion_elimination
 //
-//  Created by denis  on 31.10.2019.
+//  Created by denis  on 01.11.2019.
 //  Copyright © 2019 denis . All rights reserved.
 //
 #include <iostream>
 #include <iomanip>
 using namespace std;
 int main() {
-    int n,j,i,k;
+    int n,j,i,k,f;
     cin>>n;
-    double temp,A[n][n+1],x[n];
+    double temp,A[n][n+1],x[n+1];
     for (int i=0;i<n;i++)
         for (int j=0;j<n+1;j++)
             cin>>A[i][j];
@@ -19,26 +19,21 @@ int main() {
     for (int i =0;i<n;i++)
         if  (A[i][i]==0)
         {
-           if (i==0)
-               for (j=0;j<n+1;j++)
-               {
-                   x[j]=A[i][j];
-                   A[i][j]=A[i+1][j];
-                   A[i+1][j]=x[j];
-               }
-            else
+            f=i;
+            while (f<n and A[f][i]==0)
+                f++;
+            for (int j=i;j<n+1;j++)
             {
-                for (j=0;j<n+1;j++)
-                {
-                    x[j]=A[i][j];
-                    A[i][j]=A[i-1][j];
-                    A[i-1][j]=x[j];
-                }
+                x[j]=A[i][j];
+                A[i][j]=A[f][j];
+                A[f][j]=x[j];
             }
+            
         }
     for(j=0; j<n; j++)
        for(i=0; i<n; i++) {
-          if(i!=j) {
+          if(i!=j)
+          {
              temp=A[i][j]/A[j][j];
              for(k=0; k<n+1; k++)
                 A[i][k]=A[i][k]-temp*A[j][k];
@@ -47,10 +42,13 @@ int main() {
     for (int i=0;i<n;i++)
     {
         temp=A[i][i];
-        for (int j=0;j<n+1;j++){
-            
+        for (int j=0;j<n+1;j++)
+        {
             A[i][j]=A[i][j]/temp;
-            cout<<setw(10)<<A[i][j];
+            if (j!=0)
+                cout<<setw(13)<<A[i][j];
+            else
+                cout<<A[i][j];
         }
         cout<<endl;
     }
